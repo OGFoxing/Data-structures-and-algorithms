e9d4de32a6d82f63874f29e291119fa4db4a1d47
OGFoxing <pala4ik93@icloud.com>
2016-11-05 21:15:37 +0700

    binary search tree (without remove)

---
+++ BST/src/kz/ogfox/bst/Main.java
@@ -0,0 +1,134 @@
+package kz.ogfox.bst;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+class Node {
+	int key;
+	String name; 
+	
+	Node  leftChild;
+	Node rightChild;
+	
+	Node (int key, String name) {
+		this.key = key;
+		this.name = name;
+	}
+	public String toString() {
+		return  name+"'s" + " key = " + key; 
+		
+	}
+}
+
+public class Main {
+	Node root;
+	
+	public void addNode(int key, String name) {
+		/* create new node*/
+		Node newNode = new Node (key, name);
+		
+		/*if node not root it becomes root*/
+		if (root == null) {
+			root = newNode;
+		}
+		/*find leaf node*/
+		else {
+			Node focusNode	= root;
+			Node parent;
+			while (true) {
+				parent = focusNode;
+				/* left child */
+				if(key < focusNode.key) {
+					focusNode = focusNode.leftChild;
+					if (focusNode == null) {
+						parent.leftChild = newNode;
+						return;
+					}
+				}
+				else {
+					/* right child */
+					focusNode = focusNode.rightChild;
+					if (focusNode == null) {
+						parent.rightChild = newNode;
+						return;
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public void visitTheTree(Node focusNode) {
+		if (focusNode != null) {
+			/* visited left node */
+			visitTheTree(focusNode.leftChild);
+			/* visited right node */
+			visitTheTree(focusNode.rightChild);
+			/*visited currently focused node*/
+			System.out.println(focusNode);
+		}
+	}
+	
+	public Node findElement(int key) {
+		/* start from 1st element of the tree (root) */
+		Node focusNode = root;
+		while(focusNode.key != key) {
+			if(key < focusNode.key) {
+				focusNode = focusNode.leftChild;
+			}
+			else {
+				focusNode = focusNode.rightChild;
+			}
+			if (focusNode == null)
+				return null;
+		}
+	return focusNode;
+	}
+	
+	public Node max() {
+		Node focusNode = root;
+		if(focusNode == null) {
+			return null;
+		}
+		else {
+			while(focusNode.rightChild != null) {
+				focusNode = focusNode.rightChild;
+			}
+		}
+		return focusNode;
+	}
+	
+	public Node min() {
+		Node focusNode = root;
+		if(focusNode == null) {
+			return null;
+		}
+		else {
+			while(focusNode.leftChild != null) {
+				focusNode = focusNode.leftChild;
+			}
+		}
+		return focusNode;
+	}
+
+public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int find;
+		Main theTree = new Main();
+		
+		theTree.addNode(10, "Derek");
+		theTree.addNode(40, "Bll");
+		theTree.addNode(5, "Abrakham");
+		theTree.addNode(8, "Glen");
+		theTree.addNode(15, "Karl!");
+		theTree.visitTheTree(theTree.root);
+		System.out.println("=====================");
+		Scanner scan = new Scanner(System.in);
+		find = scan.nextInt();
+		System.out.println(theTree.findElement(find));
+		System.out.println("Max: " + theTree.max());
+		System.out.println("Min: "+ theTree.min());
+	}
+}
